fbdb8750a418d6ed6d57d87a585a6ceb85a44d7c
e1b22101 <e1b22101@oit.ac.jp>
2024-10-01 16:51:41 +0900

    ユーザ名を入力し、janken.htmlを表示する処理の実装

--- src/main/java/oit/is/z2486/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2486/kaizi/janken/controller/JankenController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class Sample21Controller {
+public class JankenController {
 
   @GetMapping("/janken")
   public String janken(@RequestParam("username") String username, Model model) {
