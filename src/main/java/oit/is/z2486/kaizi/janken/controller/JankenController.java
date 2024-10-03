c58ddb3ce65fddb28543e4ae625daa7ce8274644
e1b22101 <e1b22101@oit.ac.jp>
2024-10-03 14:39:14 +0900

    第二回課題の完成

--- src/main/java/oit/is/z2486/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2486/kaizi/janken/controller/JankenController.java
@@ -2,7 +2,10 @@ package oit.is.z2486.kaizi.janken.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -13,4 +16,27 @@ public class JankenController {
     model.addAttribute("username", username);
     return "janken"; // janken.htmlを返す
   }
+
+  @GetMapping("/janken_gu/{hand}")
+  public String janken_gu(@PathVariable("hand") String hand, ModelMap model) {
+    model.addAttribute("result", "draw");
+    model.addAttribute("myhand", hand);
+    return "janken";
+  }
+
+  @GetMapping("/janken_choki/{hand}")
+  public String janken_choki(@PathVariable("hand") String hand, ModelMap model) {
+    model.addAttribute("result", "You Lose...");
+    model.addAttribute("myhand", hand);
+    return "janken";
+  }
+
+  @GetMapping("/janken_pa/{hand}")
+  public String janken_pa(@PathVariable("hand") String hand, ModelMap model) {
+    model.addAttribute("result", "You Win!");
+    model.addAttribute("myhand", hand);
+    return "janken";
+  }
+
+
 }
