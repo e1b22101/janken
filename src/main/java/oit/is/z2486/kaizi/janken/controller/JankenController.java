9fb8be925bb148fca65c6f94cbbdac7dc42b0952
e1b22101 <e1b22101@oit.ac.jp>
2024-10-13 23:55:14 +0900

    課題の完成

--- src/main/java/oit/is/z2486/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2486/kaizi/janken/controller/JankenController.java
@@ -1,12 +1,22 @@
 package oit.is.z2486.kaizi.janken.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import oit.is.z2486.kaizi.janken.model.Entry;
 
 @Controller
+@RequestMapping("/janken")
 public class JankenController {
+
+  @Autowired
+  private Entry room;
 
   @GetMapping("/janken")
   public String janken() {
@@ -34,5 +44,12 @@ public class JankenController {
     return "janken";
   }
 
+  @GetMapping()
+  public String janken(Principal prin, ModelMap model) {
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
 
+    return "janken.html";
+  }
 }
