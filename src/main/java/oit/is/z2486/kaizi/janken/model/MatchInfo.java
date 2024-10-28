ea5b7555789c242f21144e3e0a8c31dbece5be4c
e1b22101 <e1b22101@oit.ac.jp>
2024-10-28 23:21:10 +0900

    ログイン後にアクティブな試合を表示する

--- src/main/java/oit/is/z2486/kaizi/janken/model/MatchInfo.java
+++ src/main/java/oit/is/z2486/kaizi/janken/model/MatchInfo.java
@@ -1,5 +1,49 @@
 package oit.is.z2486.kaizi.janken.model;
 
 public class MatchInfo {
-  
+  int id;
+  int user1;
+  int user2;
+  String user1Hand;
+  Boolean isActive;
+
+  public int getUser1() {
+    return user1;
+  }
+
+  public void setUser1(int user1) {
+    this.user1 = user1;
+  }
+
+  public int getUser2() {
+    return user2;
+  }
+
+  public void setUser2(int user2) {
+    this.user2 = user2;
+  }
+
+  public String getUser1Hand() {
+    return user1Hand;
+  }
+
+  public void setUser1Hand(String user1Hand) {
+    this.user1Hand = user1Hand;
+  }
+
+  public Boolean getIsActive() {
+    return isActive;
+  }
+
+  public void setIsActive(Boolean isActive) {
+    this.isActive = isActive;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
 }
